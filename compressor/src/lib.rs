5c697cbccccfc7eb6a5f68927d394e3241ff6ffb
lihka1202 <88475310+lihka1202@users.noreply.github.com>
2024-10-13 18:45:37 +0800

    chore: add basic compressing formats

--- compressor/src/lib.rs
+++ compressor/src/lib.rs
@@ -1,14 +1,4 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+use std::io::prelude::*;
+use flate2::Compression;
+use flate2::write::ZlibEncoder;
+use flate2::write::GzEncoder;
