55ad28d8817699a0e7e1fb3513ec62620bba0124
lihka1202 <88475310+lihka1202@users.noreply.github.com>
2024-10-13 18:19:13 +0800

    chore: add CLI to runner

--- runner/src/main.rs
+++ runner/src/main.rs
@@ -1,3 +1,17 @@
+use clap::Parser;
+#[derive(Parser, Debug)]
+#[command(version = "0.0.1", about = "This program can compress a file.", long_about = None)]
+struct Args {
+    /// The location of the source file
+    #[arg(short, long, help = "Location of the input file")]
+    source: String,
+    /// Location of the destination file
+    #[arg(short, long, help = "Location of the destination file")]
+    destination: String,
+
+}
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+    println!("Source: {}", args.source);
+    println!("Destination: {}", args.destination);
 }
