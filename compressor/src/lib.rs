d73f7f70fceb3e129081698ab483a7e41fd1cef1
lihka1202 <88475310+lihka1202@users.noreply.github.com>
2024-10-21 23:32:04 +0800

    feat: compare compression algorithms between GzEncoder and ZLibEncoder

--- compressor/src/lib.rs
+++ compressor/src/lib.rs
@@ -1,4 +1,21 @@
+use std::env::Args;
+use std::io::BufReader;
 use std::io::prelude::*;
 use flate2::Compression;
 use flate2::write::ZlibEncoder;
 use flate2::write::GzEncoder;
+
+pub fn encode(source: String, target: String) {
+    let metadata = std::fs::metadata(String::from("input/") + &source).unwrap();
+    println!("Length before compression: {:?}", metadata.len());
+    let mut encoder = GzEncoder::new(Vec::new(), Compression::default());
+    encoder.write_all(source.as_bytes()).unwrap();
+    let compressed = encoder.finish().unwrap();
+    println!("Length after compression (GzEncoder): {:?}", compressed.len());
+
+    // Write the ZLibEncoder code
+    let mut zlib_encoder = ZlibEncoder::new(Vec::new(), Compression::default());
+    zlib_encoder.write_all(source.as_bytes()).unwrap();
+    let compressed_zlib_encoder = zlib_encoder.finish().unwrap();
+    println!("Length after compression (ZLibEncoder): {:?}", compressed_zlib_encoder.len());
+}
