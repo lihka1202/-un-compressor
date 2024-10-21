f728c57580d817c9bcaa2b0a1e9c8ba184fa8558
lihka1202 <88475310+lihka1202@users.noreply.github.com>
2024-10-21 22:49:29 +0800

    chore: logging added to runner

--- runner/src/main.rs
+++ runner/src/main.rs
@@ -2,7 +2,7 @@ use clap::Parser;
 #[derive(Parser, Debug)]
 #[command(version = "0.0.1", about = "This program can compress a file.", long_about = None)]
 struct Args {
-    /// The location of the source file
+    /// The location of the source file, in the input directory for the time being
     #[arg(short, long, help = "Location of the input file")]
     source: String,
     /// Location of the destination file
@@ -10,8 +10,14 @@ struct Args {
     destination: String,
 
 }
-fn main() {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args = Args::parse();
+    println!("This is the current directory: {}", std::env::current_dir().unwrap().display().to_string());
     println!("Source: {}", args.source);
     println!("Destination: {}", args.destination);
+
+    let metadata = std::fs::metadata(String::from("input/") + &args.source)?;
+    println!("Length before compression: {:?}", metadata.len());
+
+    Ok(())
 }
