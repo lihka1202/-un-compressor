99511fe4acb498e4b25c52763cb8fdfbe8fcb861
lihka1202 <88475310+lihka1202@users.noreply.github.com>
2024-10-25 14:51:14 +0800

    chore: add code to get the frequency of each symbol

--- compressor/src/huffman.rs
+++ compressor/src/huffman.rs
@@ -1,4 +1,5 @@
 use std::cmp::Ordering;
+use std::collections::HashMap;
 
 #[derive(Debug, Eq, PartialEq)]
 struct HuffmanNode {
@@ -22,4 +23,12 @@ impl PartialOrd for HuffmanNode {
 
 fn new_node(symbol: Option<u8>, frequency: u32) -> Box<HuffmanNode> {
     Box::new(HuffmanNode { symbol, frequency, left: None, right: None })
+}
+
+fn count_frequency(data: &[u8]) -> HashMap<u8, u32> {
+    let mut freq_map: HashMap<u8, u32> = HashMap::new();
+    for &datum in data {
+        *freq_map.entry(datum).or_insert(0) += 1;
+    }
+    freq_map
 }
