a8f04a2e17fd60523fcf992ed9e6694f57b08b34
lihka1202 <88475310+lihka1202@users.noreply.github.com>
2024-10-25 14:37:00 +0800

    chore: add node structure + constructor

--- compressor/src/huffman.rs
+++ compressor/src/huffman.rs
@@ -1,6 +1,25 @@
+use std::cmp::Ordering;
+
+#[derive(Debug, Eq, PartialEq)]
 struct HuffmanNode {
     symbol: Option<u8>,
     frequency: u32,
     left: Option<Box<HuffmanNode>>,
     right: Option<Box<HuffmanNode>>,
+}
+
+impl Ord for HuffmanNode {
+    fn cmp(&self, other: &Self) -> Ordering {
+        other.frequency.cmp(&self.frequency)
+    }
+}
+
+impl PartialOrd for HuffmanNode {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+fn new_node(symbol: Option<u8>, frequency: u32) -> Box<HuffmanNode> {
+    Box::new(HuffmanNode { symbol, frequency, left: None, right: None })
 }
